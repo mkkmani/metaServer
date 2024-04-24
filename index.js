5af5fb5ceef9c3b69c121ef7bac59ab942393519
mani <mani@email.com>
2024-04-24 11:35:58 +0530

    meta server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors')
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+const PORT =  3000;
+
+// Proxy endpoint
+app.post('/proxy', async (req, res) => {
+  const { url } = req.body;
+
+  try {
+    const response = await axios.get(url);
+    res.send({data:response.data});
+  } catch (error) {
+    res.status(500).send('Error fetching URL');
+  }
+});
+
+// app.listen(PORT, () => {
+//   console.log(`Proxy server is running on port ${PORT}`);
+// });
+
+app.listen(PORT)
